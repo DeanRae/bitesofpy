ef4ebf95f0b6f03a4c2e9c0c45a879404323c8ed
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-11 23:57:39 +1300

    implement marking pybites birthday

--- 16/gendates.py
+++ 16/gendates.py
@@ -7,8 +7,13 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 def gen_special_pybites_dates():
     marked_dates = []
     current_date = PYBITES_BORN
+    next_birthday = PYBITES_BORN.replace(year=PYBITES_BORN.year + 1)
 
     # for this bite, we stop at 2020
     while current_date.year < 2020:
         current_date += timedelta(days=100)
+
+        if current_date.year > next_birthday.year:
+            yield next_birthday
+            next_birthday = next_birthday.replace(year=next_birthday.year + 1)
         yield current_date
