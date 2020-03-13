332c96919be3bcf30ffca1615c182948b53aa178
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-13 21:42:01 +1300

    use list comprehension instead of for loop for get_first_model_each_manufacturer()

--- 21/cars.py
+++ 21/cars.py
@@ -15,10 +15,7 @@ def get_all_jeeps(cars=cars):
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    first_models = []
-    for manufacturer in cars.values():
-        first_models.append(manufacturer[0])
-    return first_models
+    return [manufacturer[0] for manufacturer in cars.values()]
 
 
 def get_all_matching_models(cars=cars, grep="trail"):
