1b321801f0a0788326f520f3ef4c094eea40e3b6
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-16 00:45:31 +1300

    complete bite using two separate lists to differentiate between alphanumeric and non

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,18 @@
 def get_index_different_char(chars):
-    pass
+    """ store two separate lists of non-alpha and alphanumeric characters. Its values are the indexes of the given chars array. Returns the first element of the shorter list as this is the one containing the odd element """
+
+    non_alnum_chars = []
+    alnum_chars = []
+
+    for index, char in enumerate(chars):
+        if str(char).isalnum():
+            alnum_chars.append(index)
+        else:
+            non_alnum_chars.append(index)
+
+    return (
+        alnum_chars[0]
+        if len(alnum_chars) < len(non_alnum_chars)
+        else non_alnum_chars[0]
+    )
+
