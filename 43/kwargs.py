47ac0367e36c79c097c13c941e2a0adfffb3c3b3
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-18 21:28:02 +1300

    complete get_profile() with kwargs

--- 43/kwargs.py
+++ 43/kwargs.py
@@ -1,1 +1,3 @@
-def get_profile(...
+def get_profile(*, name="julian", profession="programmer"):
+    # refer to this for explanations on keyword arguments: https://treyhunner.com/2018/04/keyword-arguments-in-python/
+    return f"{name} is a {profession}"
