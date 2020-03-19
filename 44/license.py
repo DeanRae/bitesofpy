77b9480d89ed08b8a28aceeb728a07d381f4bd4e
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-19 22:15:32 +1300

    complete helper function using secrets module to generate random characters

--- 44/license.py
+++ 44/license.py
@@ -9,4 +9,6 @@ def gen_key(parts=4, chars_per_part=8):
 
 
 def _gen_random_alphanumeric_seq(num_characters):
-    pass
+    """ helper function for gen_key which returns a random string of letters and
+    numbers with length num_characters """
+    return "".join(secrets.choice(alphabet) for i in range(num_characters))
