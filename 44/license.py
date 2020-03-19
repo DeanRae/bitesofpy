ec87e0b6aa921714f57b1cc670471dea55f5e9a0
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-19 22:14:51 +1300

    add alphabet that contains all the valid characters used in the key

--- 44/license.py
+++ 44/license.py
@@ -1,12 +1,12 @@
 import secrets
 import string
 
+alphabet = string.ascii_uppercase + string.digits
+
 
 def gen_key(parts=4, chars_per_part=8):
     pass
 
 
 def _gen_random_alphanumeric_seq(num_characters):
-    """ helper function for gen_key which returns a random string of letters and
-    numbers with length num_characters """
     pass
