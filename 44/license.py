45d0ef2901f5881c25b483d5d447a450d0afa9d9
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-19 22:15:58 +1300

    complete gen_key()

--- 44/license.py
+++ 44/license.py
@@ -5,7 +5,11 @@ alphabet = string.ascii_uppercase + string.digits
 
 
 def gen_key(parts=4, chars_per_part=8):
-    pass
+    """ generates n parts of random sequence of upper case letters and numbers
+    each with length of chars_per_part and separated with '-' """
+    return "-".join(
+        [_gen_random_alphanumeric_seq(chars_per_part) for i in range(parts)]
+    )
 
 
 def _gen_random_alphanumeric_seq(num_characters):
