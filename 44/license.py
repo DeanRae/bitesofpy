216fb0ca40259544f5e120f9770c7fe7da7d58e2
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2020-03-19 21:47:32 +1300

    create helper function to generate random string of letters and characters

--- 44/license.py
+++ 44/license.py
@@ -4,3 +4,9 @@ import string
 
 def gen_key(parts=4, chars_per_part=8):
     pass
+
+
+def _gen_random_alphanumeric_seq(num_characters):
+    """ helper function for gen_key which returns a random string of letters and
+    numbers with length num_characters """
+    pass
